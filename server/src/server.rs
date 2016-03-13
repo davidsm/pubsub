8129f7863fad1d3ecfd8966487db7cb9a0424bee
David <david.smitmanis@gmail.com>
2016-03-13 21:21:54 +0100

    Stub implementation of connection ready state

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,18 +1,20 @@
 use mio::tcp::{TcpListener, TcpStream};
 use mio::util::Slab;
-use mio::{EventSet, PollOpt};
+use mio::{EventSet, PollOpt, TryRead};
 use mio;
+
+use std::str;
 
 const SERVER: mio::Token = mio::Token(0);
 
-struct Connection {
+struct PubsubClient {
     socket: TcpStream,
     token: mio::Token
 }
 
-impl Connection {
-    pub fn new(socket: TcpStream, token: mio::Token) -> Connection {
-        Connection {
+impl PubsubClient {
+    pub fn new(socket: TcpStream, token: mio::Token) -> PubsubClient {
+        PubsubClient {
             socket: socket,
             token: token
         }
@@ -21,11 +23,22 @@ impl Connection {
     pub fn socket(&self) -> &TcpStream {
         &self.socket
     }
+
+    pub fn ready(&mut self, event_loop: &mut mio::EventLoop<PubsubServer>) {
+        let mut buf = [0; 128];
+        match self.socket.try_read(&mut buf) {
+            Ok(Some(len)) => println!("{:?}", str::from_utf8(&buf[..len]).unwrap()),
+            Ok(None) => println!("Got None while reading"),
+            Err(e) => println!("{}", e)
+        }
+        event_loop.reregister(&self.socket, self.token, EventSet::readable(),
+                              PollOpt::edge() | PollOpt::oneshot()).unwrap();
+    }
 }
 
 pub struct PubsubServer {
     socket: TcpListener,
-    connections: Slab<Connection>
+    connections: Slab<PubsubClient>
 }
 
 impl PubsubServer {
@@ -61,7 +74,7 @@ impl mio::Handler for PubsubServer {
                 };
 
                 let token = self.connections.insert_with(|token| {
-                    Connection::new(client_socket, token)
+                    PubsubClient::new(client_socket, token)
                 }).expect("Failed to insert new connection");
 
                 event_loop.register(self.connections[token].socket(), token,
@@ -71,7 +84,9 @@ impl mio::Handler for PubsubServer {
 
             },
 
-            _ => unreachable!()
+            _ => {
+                self.connections[token].ready(event_loop);
+            }
         }
     }
 }
